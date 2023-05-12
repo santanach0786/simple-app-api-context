dd2718194282efe421252e86a3ff4ed18179d7d8
santanach0786 <santanach0786@gmail.com>
2023-05-12 10:01:09 -0400

    ESTA ES UNA PRUEBA PARA USAR APICONTEXT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Components/Button";
+import Page1 from "./Components/Page-1";
+import Page2 from "./Components/Page-2";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola Mundo</h1>
+      <Page1/>
+      <Page2/>  
+      <Button/>    
     </div>
   );
 }
---
+++ src/Components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button>Change Props</button>
+  )
+}
+
+export default Button
